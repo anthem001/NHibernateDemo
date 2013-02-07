3ac6327795b0c9bc9974e0a027c75f8e7f1a0d25
anthem001 <anthem001@gmail.com>
2013-02-07 00:14:18 -0700

    initial commit

---
+++ NHibernateDemo/Customers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NHibernateDemo
+{
+    class Customers
+    {
+        public virtual int CustomerId { get; set; }
+        public virtual int NameStyle { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string MiddleName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string Suffix { get; set; }
+        public virtual string CompanyName { get; set; }
+        public virtual string SalesPerson { get; set; }
+        public virtual string EmailAddress { get; set; }
+        public virtual string Phone { get; set; }
+        public virtual string PasswordHash { get; set; }
+        public virtual string PasswordSalt { get; set; }
+        public virtual Guid Rowguid { get; set; }
+        public virtual DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ NHibernateDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+
+namespace NHibernateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* NHibernate Config Object */
+            var config = new Configuration();
+
+            /* Configure Database Connection */
+            config.DataBaseIntegration(c =>
+                {
+                    c.ConnectionString =
+                        "Server=USER-09167EE968;Database=AdventureWorksLT2008R2;User Id=kelly;Password=test;";
+                    c.Driver<SqlClientDriver>();
+                    c.Dialect<MsSql2008Dialect>();
+                });
+
+            /* Get executing assembly to search for mapping files */
+            config.AddAssembly(Assembly.GetExecutingAssembly());
+
+            /* Compile Meta Data To Initialize NHibernate */
+            var sessionConnection = config.BuildSessionFactory();
+
+            /* Build a session (database connection) */
+            using(var session = sessionConnection.OpenSession())
+
+            /* Begin a new transaction */
+            using (var transaction = session.BeginTransaction())
+            {
+                //run some database stuff
+                var customers = session.CreateCriteria<Customers>().List<Customers>();
+
+                //iterate through customers
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine("{0} {1} {2} {3}", customer.Title, customer.FirstName, customer.LastName, customer.Suffix);
+                }
+
+                //pause for view
+                Console.ReadLine();
+                //commit transaction
+                transaction.Commit();
+            }
+
+        }
+    }
+}
